eef32ccc25b0961345fc72eeda7d9873192fd1b6
Blarce <119196285+Blarce@users.noreply.github.com>
2025-04-06 16:05:00 +0300

    Update Nullable_types.kt

--- Introduction/Code/Nullable_types.kt
+++ Introduction/Code/Nullable_types.kt
@@ -1,1 +1,18 @@
+fun sendMessageToClient(
+        client: Client?, message: String?, mailer: Mailer
+) {
+     val personalInfo: PersonalInfo? = client?.personalInfo;
+    val email: String? = personalInfo?.email;
+    if(email == null || message == null)
+    {
+        return;
+    }
+    
+    mailer.sendMessage(email, message);
+}
 
+class Client(val personalInfo: PersonalInfo?)
+class PersonalInfo(val email: String?)
+interface Mailer {
+    fun sendMessage(email: String, message: String)
+}
