e458d29db839f431e485472eeb1470d5c31ab722
Blarce <119196285+Blarce@users.noreply.github.com>
2025-04-10 16:20:38 +0300

    Update Fold_and_reduce.kt

--- Collections/Code/Fold_and_reduce.kt
+++ Collections/Code/Fold_and_reduce.kt
@@ -1,8 +1,7 @@
-//Return the set of products that were ordered by all customers
-fun Shop.getProductsOrderedByAll() SetProduct =
-    customers.map(CustomergetOrderedProducts).reduce { orderedByAll, customer -
+// Return the set of products that were ordered by all customers
+fun Shop.getProductsOrderedByAll(): Set<Product> = customers.map(Customer::getOrderedProducts).reduce { orderedByAll, customer ->
         orderedByAll.intersect(customer)
-    };
+    }
 
-fun Customer.getOrderedProducts() SetProduct =
-    orders.flatMap(Orderproducts).toSet();
+fun Customer.getOrderedProducts(): Set<Product> =
+   orders.flatMap(Order::products).toSet()
